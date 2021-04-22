d6e96c030577a05e040d82bfdb20fa7156ba74b0
yogendra jain <yogendrajain64@gmail.com>
2021-04-22 14:49:10 +0530

    first stage commit

--- src/App.js
+++ src/App.js
@@ -1,80 +1,100 @@
-// @flow
-import * as React from 'react';
+import React from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 import './App.css';
-import {Header} from './components/Header';
-import {Sidebar} from './components/Sidebar';
-import {AppProvider, useAppContext} from './context/AppContext';
-import {ProductResults} from './components/ProductResults';
-import {ProductDetails} from './components/ProductDetails';
-import {Modal} from './components/Modal';
-import {getCategories, getProduct} from './requests';
-// import type {Category, Product} from './types';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+import CartScreen from './screens/CartScreen';
+import SigninScreen from './screens/SigninScreen';
+import { useSelector } from 'react-redux';
+import RegisterScreen from './screens/RegisterScreen';
+import ProductsScreen from './screens/ProductsScreen';
+import ShippingScreen from './screens/ShippingScreen';
+import PaymentScreen from './screens/PaymentScreen';
+import PlaceOrderScreen from './screens/PlaceOrderScreen';
+import OrderScreen from './screens/OrderScreen';
+import ProfileScreen from './screens/ProfileScreen';
+import OrdersScreen from './screens/OrdersScreen';
+
+import axios from 'axios';
+
+axios.defaults.baseURL = 'http://parassaree.herokuapp.com/' // the prefix of the URL
+axios.defaults.headers.get['Accept'] = 'application/json'   // default header for all get request
+axios.defaults.headers.post['Accept'] = 'application/json'  // default header for all POST request
 
 function App() {
-  useAppEffect();
-  const [{activeProduct}, dispatch] = useAppContext();
+  const userSignin = useSelector((state) => state.userSignin);
+  const { userInfo } = userSignin;
 
+  const openMenu = () => {
+    document.querySelector('.sidebar').classList.add('open');
+  };
+  const closeMenu = () => {
+    document.querySelector('.sidebar').classList.remove('open');
+  };
   return (
-    <div>
-      <Header title="Paras Sarees" />
-      <div className="container container--primary">
-        <Sidebar />
-        <ProductResults />
-        <Modal
-          isOpen={Boolean(activeProduct)}
-          close={() => dispatch({type: 'DESELECT_PRODUCT'})}
-        >
-          {activeProduct && <ProductDetails product={activeProduct} />}
-        </Modal>
+    <BrowserRouter>
+      <div className="grid-container">
+        <header className="header">
+          <div className="brand">
+            <button onClick={openMenu}>&#9776;</button>
+            <Link to="/">Paras Sarees</Link>
+          </div>
+          <div className="header-links">
+            <a href="cart.html">Cart</a>
+            {userInfo ? (
+              <Link to="/profile">{userInfo.name}</Link>
+            ) : (
+              <Link to="/signin">Sign In</Link>
+            )}
+            {userInfo && userInfo.isAdmin && (
+              <div className="dropdown">
+                <a href="#">Admin</a>
+                <ul className="dropdown-content">
+                  <li>
+                    <Link to="/orders">Orders</Link>
+                    <Link to="/products">Products</Link>
+                  </li>
+                </ul>
+              </div>
+            )}
+          </div>
+        </header>
+        <aside className="sidebar">
+          <h3>Shopping Categories</h3>
+          <button className="sidebar-close-button" onClick={closeMenu}>
+            x
+          </button>
+          <ul className="categories">
+            <li>
+              <Link to="/category/Pants">Pants</Link>
+            </li>
+
+            <li>
+              <Link to="/category/Shirts">Shirts</Link>
+            </li>
+          </ul>
+        </aside>
+        <main className="main">
+          <div className="content">
+            <Route path="/orders" component={OrdersScreen} />
+            <Route path="/profile" component={ProfileScreen} />
+            <Route path="/order/:id" component={OrderScreen} />
+            <Route path="/products" component={ProductsScreen} />
+            <Route path="/shipping" component={ShippingScreen} />
+            <Route path="/payment" component={PaymentScreen} />
+            <Route path="/placeorder" component={PlaceOrderScreen} />
+            <Route path="/signin" component={SigninScreen} />
+            <Route path="/register" component={RegisterScreen} />
+            <Route path="/product/:id" component={ProductScreen} />
+            <Route path="/cart/:id?" component={CartScreen} />
+            <Route path="/category/:id" component={HomeScreen} />
+            <Route path="/" exact={true} component={HomeScreen} />
+          </div>
+        </main>
+        <footer className="footer">All right reserved.</footer>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
-function useAppEffect() {
-  const [state, dispatch] = useAppContext();
-
-  // Request categories
-  React.useEffect(
-    () => {
-      getCategories().then((data) => {
-        dispatch({
-          type: 'FETCH_CATEGORIES',
-          status: 'success',
-          data,
-        });
-      });
-    },
-    [dispatch]
-  );
-
-  // Request product when product selected
-  const {activeProductId} = state;
-  React.useEffect(
-    () => {
-      if (!activeProductId) {
-        return;
-      }
-      dispatch({
-        type: 'FETCH_PRODUCT',
-        status: 'begin',
-        id: activeProductId,
-      });
-      getProduct(activeProductId).then((data) => {
-        dispatch({
-          type: 'FETCH_PRODUCT',
-          status: 'success',
-          id: activeProductId,
-          data,
-        });
-      });
-    },
-    [activeProductId, dispatch]
-  );
-}
-
-export default () => (
-  <AppProvider>
-    <App />
-  </AppProvider>
-);
+export default App;
